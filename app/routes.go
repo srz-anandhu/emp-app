d425ea82cf29258b98ce70fb1cfd3a2e08b415e6
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2025-05-06 15:28:38 +0530

    added ApiRoute function to initialize repo, service, controller layer and setted routes

--- app/routes.go
+++ app/routes.go
@@ -1,1 +1,27 @@
 package app
+
+import (
+	"emp-app/app/controller"
+	"emp-app/app/repository"
+	"emp-app/app/service"
+
+	"github.com/go-chi/chi/v5"
+	"gorm.io/gorm"
+)
+func ApiRoute(db *gorm.DB) chi.Router {
+
+	empRepo := repository.NewEmployeeRepo(db)
+	empService := service.NewEmployeeService(empRepo)
+	empController := controller.NewEmployeeController(empService)
+
+	r := chi.NewRouter()
+
+	r.Route("/employee", func(r chi.Router) {
+		r.Post("/create", empController.CreateEmployee)
+		r.Put("/{id}", empController.UpdateEmployee)
+		r.Get("/{id}", empController.GetEmployee)
+		r.Get("/", empController.GetAllEmployees)
+	})
+
+	return r
+}
