a39a9341afb9debcb870ad4fe876816299ef801b
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2025-05-07 11:19:51 +0530

    password hashing and comparison

--- pkg/helpers/hash/hashing.go
+++ pkg/helpers/hash/hashing.go
@@ -1,1 +1,23 @@
 package hash
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytePass, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password")
+	}
+
+	return string(bytePass), nil
+}
+
+func ComparePassword(password, hashedPassword string) (bool, error) {
+	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)); err != nil {
+		return false, fmt.Errorf("password mismatch : %w", err)
+	}
+	return true, nil
+}
