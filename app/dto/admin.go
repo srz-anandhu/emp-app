0e1cfe171f8a57cd0ca9b02e9fc352c908e404b7
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2025-07-09 17:59:30 +0530

    removed required tag from employee id from dto

--- app/dto/admin.go
+++ app/dto/admin.go
@@ -44,7 +44,7 @@ type AdminLoginResponse struct {
 
 // Employee management field for Admins
 type AddEmployeeDetails struct {
-	EmployeeID string      `json:"employee_id" validate:"required"`
+	EmployeeID string      `json:"employee_id"`
 	FullName   string      `json:"name"`
 	Email      string      `json:"email"`
 	Password   string      `json:"password"`
